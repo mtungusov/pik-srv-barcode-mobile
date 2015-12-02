924bec4cd73a41ab131e73293ca63baab028648a
TM <tm@macadmin.ru>
2015-12-02 16:52:44 +0300

    add worker ConsumerTest

--- lib/workers.rb
+++ lib/workers.rb
@@ -3,3 +3,4 @@ require 'kafka/producer'
 
 module Workers; end
 require 'workers/producer_random'
+require 'workers/consumer_test'
---
+++ lib/workers/consumer_test.rb
@@ -0,0 +1,32 @@
+class Workers::ConsumerTest
+  include Celluloid
+  include Celluloid::Internals::Logger
+
+  finalizer :shutdown
+
+  attr_reader :consumer
+
+  def initialize
+    info "ConsumerTest starting up..."
+    @consumer = Kafka::Consumer.new (
+                                      {
+                                        'bootstrap.servers'=>'kafka.dev:9092',
+                                        'group.id'=>'test-ruby-consumer'
+                                      }
+                                    )
+    consumer.subscribe [$config['kafka']['topic']]
+  end
+
+  def process
+    loop do
+      consumer.poll.each do |record|
+        info "value: #{record.value}, offset: #{record.offset}"
+      end
+      sleep 0.1
+    end
+  end
+
+  def shutdown
+    consumer.close
+  end
+end
