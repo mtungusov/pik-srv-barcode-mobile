77da17734b5b702c2ad5ba9a87fb4c2300e337fe
TM <tm@macadmin.ru>
2015-12-21 15:30:41 +0300

    change start.sh script

--- bin/start.sh
+++ bin/start.sh
@@ -3,6 +3,11 @@
 # Run: ./bin/start.sh
 ###
 
+if [ "x$RUN_ENV" = "x" ]
+then
+  RUN_ENV="development"
+fi
+
 LIBPATH=("${HOME}"/opt/kafka_2.11-0.9.0.0/libs/*.jar)
 for i in "${LIBPATH[@]}"
 do
@@ -10,4 +15,4 @@ do
 done
 
 export CLASSPATH=$CLASSPATH
-export BARCODE_MOBILE_ENV=development && ./bin/srv-start
+export BARCODE_MOBILE_ENV=$RUN_ENV && ./bin/srv-start
