71109320506a00541c052357077c1ff02a48d46c
TM <tm@macadmin.ru>
2016-02-19 10:34:22 +0300

    init grape branch

--- config/config.rb
+++
@@ -1,8 +0,0 @@
-require 'yaml'
-
-module Configuration
-  def self.load_config(env)
-    config_file = 'config/config.yml'
-    File.exist?(config_file) ? YAML.load_file(config_file)[env] : {}
-  end
-end
