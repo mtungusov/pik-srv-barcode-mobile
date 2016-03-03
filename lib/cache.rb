88e0fd84a368a100cb1eaf5fdff397816fac1fc4
TM <tm@macadmin.ru>
2016-03-03 14:13:38 +0300

    remove webmachine version

--- lib/cache.rb
+++
@@ -1,9 +0,0 @@
-require 'cache/redis_connection'
-
-module Cache
-  module_function
-
-  def pool(options={})
-    @cache_pool ||= Cache::RedisConnection.create options
-  end
-end
--- lib/workers.rb
+++
@@ -1,6 +0,0 @@
-require 'kafka/producer'
-require 'kafka/consumer'
-
-module Workers; end
-require 'workers/producer_random'
-require 'workers/consumer_test'
